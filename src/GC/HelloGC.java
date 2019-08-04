351ad7696ee12fc826c850eeff9a4e86d0cb146f
wangqiang <850994281@qq.com>
2019-08-04 13:15:32 +0800

    Gc

---
+++ src/GC/HelloGC.java
@@ -0,0 +1,21 @@
+package GC;
+
+public class HelloGC {
+    public static void main(String[] args) {
+//        System.out.println("hello GC");
+//        try {
+//            Thread.sleep(Integer.MAX_VALUE);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        long totalMemory = Runtime.getRuntime().totalMemory();
+//        long maxMemory = Runtime.getRuntime().maxMemory();
+//        System.out.println("TOTALMEMORY(-Xms):"+totalMemory+"字节，"+(totalMemory/(double)1024/1024)+"MB");
+//        System.out.println("MAXMEMORY(-Xmx):"+maxMemory+"字节，"+(maxMemory/(double)1024/1024)+"MB");
+
+
+        Byte[] bytes=new Byte[50*1024*1024];
+
+    }
+}
